217f72294b9daeb121ae991d6d479bf46d0fc400
zhifez <zhifez.lee@gmail.com>
2021-09-19 15:34:56 +0800

    - Fixed postcss issue

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,13 +1,6 @@
-// module.exports = {
-//     plugins: {
-//         tailwindcss: {},
-//         autoprefixer: {},
-//     }
-// }
 module.exports = {
-    plugins: [
-      "postcss-import",
-      "tailwindcss",
-      "autoprefixer"
-    ]
-};
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    }
+}
